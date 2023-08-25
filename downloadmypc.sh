a1e981abea94bc771e9b4136f893ad4f0422fb01
Love-Ink <2893689152@qq.com>
2023-08-25 17:23:16 +0800

    test

---
+++ downloadmypc.sh
@@ -0,0 +1,7 @@
+firmwareFile=$(echo $1 | sed 's/^\([a-zA-Z]\)\(.*\)/\2/g; s/://g' | sed 's/\\/\//g')
+d:/Program_Files/MounRiver/MounRiver_Studio/toolchain/OpenOCD/bin/openocd.exe -f d:/Program_Files/MounRiver/MounRiver_Studio/toolchain/OpenOCD/bin/wch-riscv.cfg \
+    -c init -c halt \
+    -c "flash erase_sector wch_riscv 0 last" \
+    -c "program ${firmwareFile}" \
+    -c "verify_image ${firmwareFile}" \
+    -c wlink_reset_resume -c exit
